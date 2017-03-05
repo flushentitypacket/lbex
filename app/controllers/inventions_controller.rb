501da438afe343f1f9fb558fca29b692c5e7592e
Ronald Hong <ron@wistia.com>
2017-03-05 18:20:26 -0500

    Add bits and materials assocation with invention json payload.

--- app/controllers/inventions_controller.rb
+++ app/controllers/inventions_controller.rb
@@ -1,10 +1,12 @@
 class InventionsController < ApplicationController
+  PAYLOAD_ITEMS_LIMIT = 100
+
   def index
     respond_to do |format|
       format.html
       format.json do
-        inventions = Invention.all
-        render :json => inventions
+        inventions = Invention.limit(PAYLOAD_ITEMS_LIMIT).includes(:bits, :materials)
+        render :json => inventions, include: [:bits, :materials]
       end
     end
   end
--- spec/controllers/inventions_controller_spec.rb
+++ spec/controllers/inventions_controller_spec.rb
@@ -10,17 +10,42 @@ RSpec.describe InventionsController, type: :controller do
     end
 
     context 'json' do
+      let(:title) { 'maikoolInventshin' }
+      let(:bit_name) { 'wowwhatabit' }
+      let(:material_name) { 'andafinematerial' }
+      let(:invention) do
+        Invention.new(title: title).tap do |i|
+          i.bits.build(name: bit_name)
+          i.materials.build(name: material_name)
+        end
+      end
+
       it 'returns http success' do
         get :index, { format: :json }
         expect(response).to have_http_status(:success)
       end
 
       it 'returns inventions serialized as json' do
-        title = 'maikoolInventshin'
-        Invention.new(title: title).save!
+        invention.save!
         get :index, { format: :json }
         parsed = JSON.parse(response.body)
         expect(parsed.first['title']).to eq(title)
+      end
+
+      it 'includes serialized bits' do
+        invention.save!
+        get :index, { format: :json }
+        parsed = JSON.parse(response.body)
+        bit = parsed.first['bits'].first
+        expect(bit['name']).to eq(bit_name)
+      end
+
+      it 'includes serialized materials' do
+        invention.save!
+        get :index, { format: :json }
+        parsed = JSON.parse(response.body)
+        material = parsed.first['materials'].first
+        expect(material['name']).to eq(material_name)
       end
     end
   end
