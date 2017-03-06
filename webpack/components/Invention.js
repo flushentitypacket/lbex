9c3d1a71c7b1d677fe0428ce3dfcd759af2d8d54
Ronald Hong <ron@wistia.com>
2017-03-05 22:10:34 -0500

    Show invention attributes.

--- webpack/components/Invention.js
+++ webpack/components/Invention.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 class Invention extends React.Component {
-  render = () => <div>invention</div>;
+  static propTypes = {
+    id: PropTypes.number,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    username: PropTypes.string,
+    email: PropTypes.string,
+    bits: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string,
+    })),
+    materials: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string,
+    })),
+  };
+
+  static defaultProps = {
+    bits: [],
+    materials: [],
+  };
+
+  render = () => {
+    const { id, title, description, username, email, bits, materials } = this.props;
+    return (
+      <div>
+        <p><strong>id</strong> {id}</p>
+        <p><strong>title</strong> {title}</p>
+        <p><strong>description</strong> {description}</p>
+        <p><strong>username</strong> {username}</p>
+        <p><strong>email</strong> {email}</p>
+        <p><strong>bits</strong> {bits.map(bit => bit.name).join(', ')}</p>
+      </div>
+    );
+  }
 }
 
 export default Invention;
---
+++ webpack/components/Invention.spec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import expect from 'expect';
+import { mount } from 'enzyme';
+import { Invention} from './Invention';
+
+const getMounted = (props) => <Invention {...props} />;
+
+describe('Invention', () => {
+});
